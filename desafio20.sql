341f45390fd960689e10091fd65501ba4335e07c
Joao Peixoto <joao.peixoto678@gmail.com>
2023-03-30 15:57:23 -0300

    Requisitos 19 e 20 passando

---
+++ desafio20.sql
@@ -0,0 +1,2 @@
+INSERT INTO order_details (order_id, product_id, unit_price, quantity, discount, purchase_order_id, status_id, date_allocated,inventory_id)
+VALUES(69,80,15.000,15.000,0,NULL,2,NULL,129);
